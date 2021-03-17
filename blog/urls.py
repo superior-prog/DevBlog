a4b2ef1bc58dc6959396b4cb0d05bdc5a92dcf4c
Meheraj <57069206+superior-prog@users.noreply.github.com>
2021-03-17 12:16:20 +0600

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -4,12 +4,12 @@ from .views import *
 urlpatterns = [
     # blog urls
     path('', blog_home_view, name='blog-home'),
-    path('blogs/<str:pk>/', blogs_view, name='blogs'),
-    path('post/<str:slug>/', blog_details_view, name='blog-details'),
-    path('add-blog', add_blog_view, name='add-blog'),
-    path('edit-blog/<str:slug>/', edit_blog_view, name='edit-blog'),
-    path('delete-blog/<str:slug>/', delete_blog_view, name='delete-blog'),
-    path('edit-comment/<str:pk>/', edit_comment_view, name='edit-comment'),
-    path('delete-comment/<str:pk>/', delete_comment_view, name='delete-comment'),
+    path('blog/user/<str:pk>/', blogs_view, name='blogs'),
+    path('blog/post/<str:slug>/', blog_details_view, name='blog-details'),
+    path('blog/add-blog', add_blog_view, name='add-blog'),
+    path('blog/edit-blog/<str:slug>/', edit_blog_view, name='edit-blog'),
+    path('blog/delete-blog/<str:slug>/', delete_blog_view, name='delete-blog'),
+    path('blog/edit-comment/<str:pk>/', edit_comment_view, name='edit-comment'),
+    path('blog/delete-comment/<str:pk>/', delete_comment_view, name='delete-comment'),
 ]
 
